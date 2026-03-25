64c7f9f6e5fee7ca970a6bc4968805f4663fdb11
BenBiggen <benjamin.storesund@gmail.com>
2026-03-25 13:28:26 +0100

    25 mars

--- Gambling Maskin 6700 2.0/script.js
+++ Gambling Maskin 6700 2.0/script.js
@@ -1,60 +1,104 @@
-/*let deck = [
-  { card: "As", value: 11 }, { card: "Ks", value: 10 }, { card: "Qs", value: 10 }, { card: "Js", value: 10 }, { card: "Ts", value: 10 }, { card: "9s", value: 9 }, { card: "8s", value: 8 }, { card: "7s", value: 7 }, { card: "6s", value: 6 }, { card: "5s", value: 5 }, { card: "4s", value: 4 }, { card: "3s", value: 3 }, { card: "2s", value: 2 },
-
-  { card: "Ah", value: 11 }, { card: "Kh", value: 10 }, { card: "Qh", value: 10 }, { card: "Jh", value: 10 }, { card: "Th", value: 10 }, { card: "9h", value: 9 }, { card: "8h", value: 8 }, { card: "7h", value: 7 }, { card: "6h", value: 6 }, { card: "5h", value: 5 }, { card: "4h", value: 4 }, { card: "3h", value: 3 }, { card: "2h", value: 2 },
-
-  { card: "Ad", value: 11 }, { card: "Kd", value: 10 }, { card: "Qd", value: 10 }, { card: "Jd", value: 10 }, { card: "Td", value: 10 }, { card: "9d", value: 9 }, { card: "8d", value: 8 }, { card: "7d", value: 7 }, { card: "6d", value: 6 }, { card: "5d", value: 5 }, { card: "4d", value: 4 }, { card: "3d", value: 3 }, { card: "2d", value: 2 },
-
-  { card: "Ac", value: 11 }, { card: "Kc", value: 10 }, { card: "Qc", value: 10 }, { card: "Jc", value: 10 }, { card: "Tc", value: 10 }, { card: "9c", value: 9 }, { card: "8c", value: 8 }, { card: "7c", value: 7 }, { card: "6c", value: 6 }, { card: "5c", value: 5 }, { card: "4c", value: 4 }, { card: "3c", value: 3 }, { card: "2c", value: 2 }
-];*/
-
-let deck = [
-  "As","Ks","Qs","Js","Ts","9s","8s","7s","6s","5s","4s","3s","2s",
-  "Ah","Kh","Qh","Jh","Th","9h","8h","7h","6h","5h","4h","3h","2h",
-  "Ad","Kd","Qd","Jd","Td","9d","8d","7d","6d","5d","4d","3d","2d",
-  "Ac","Kc","Qc","Jc","Tc","9c","8c","7c","6c","5c","4c","3c","2c"
+let deck = [ 
+"As","Ks","Qs","Js","Ts","9s","8s","7s","6s","5s","4s","3s","2s",
+"Ah","Kh","Qh","Jh","Th","9h","8h","7h","6h","5h","4h","3h","2h",
+"Ad","Kd","Qd","Jd","Td","9d","8d","7d","6d","5d","4d","3d","2d",
+"Ac","Kc","Qc","Jc","Tc","9c","8c","7c","6c","5c","4c","3c","2c" 
 ];
+
+const deckValue = {
+  "As": 11, "Ks": 10, "Qs": 10, "Js": 10, "Ts": 10, "9s": 9, "8s": 8, "7s": 7, "6s": 6, "5s": 5, "4s": 4, "3s": 3, "2s": 2,
+  "Ah": 11, "Kh": 10, "Qh": 10, "Jh": 10, "Th": 10, "9h": 9, "8h": 8, "7h": 7, "6h": 6, "5h": 5, "4h": 4, "3h": 3, "2h": 2,
+  "Ad": 11, "Kd": 10, "Qd": 10, "Jd": 10, "Td": 10, "9d": 9, "8d": 8, "7d": 7, "6d": 6, "5d": 5, "4d": 4, "3d": 3, "2d": 2,
+  "Ac": 11, "Kc": 10, "Qc": 10, "Jc": 10, "Tc": 10, "9c": 9, "8c": 8, "7c": 7, "6c": 6, "5c": 5, "4c": 4, "3c": 3, "2c": 2
+};
 
 let hand = [];
 let house = [];
 const handDiv = document.getElementById("handDiv");
 const houseDiv = document.getElementById("houseDiv");
 const hitButton = document.getElementById("hit");
-const standButton = document.getElementById("stand")
+const standButton = document.getElementById("stand");
+const sumSide = {
+  house: document.getElementById("houseSum"),
+  hand: document.getElementById("handSum"),
+};
 
 hitButton.addEventListener("click", () => {
-    hit();
+  hit();
 });
 
 standButton.addEventListener("click", () => {
-    stand();
+  stand();
 });
 
-function cardPull(position, positionDiv){
-    let randomHand = deck[Math.floor(Math.random()*deck.length)];
-    let indexHand = deck.indexOf(randomHand);
-    deck.splice(indexHand, 1);
-    position.push(randomHand);
+function cardPull(position, positionDiv) {
+  let randomHand = deck[Math.floor(Math.random() * deck.length)];
+  let indexHand = deck.indexOf(randomHand);
+  deck.splice(indexHand, 1);
+  position.push(randomHand);
 
-    let handSide = document.createElement("playing-card");
-    handSide.setAttribute("cid", `${randomHand}`);
-    positionDiv.appendChild(handSide);
+  let handSide = document.createElement("playing-card");
+  handSide.setAttribute("cid", `${randomHand}`);
+  positionDiv.appendChild(handSide);
 }
 
 cardPull(hand, handDiv);
 
-cardPull(house, houseDiv)
+cardPull(house, houseDiv);
 
 cardPull(hand, handDiv);
 
-function hit(){
-    cardPull(hand, handDiv);
-};
+let randomHouse = deck[Math.floor(Math.random() * deck.length)];
+let indexHouse = deck.indexOf(randomHouse);
+deck.splice(indexHouse, 1);
+house.push(randomHouse);
 
-function stand(){
-    cardPull(house, houseDiv)
+let handSide = document.createElement("playing-card");
+handSide.setAttribute("rank", `0`);
+houseDiv.appendChild(handSide);
+
+sumSide.house.innerText = sumHand(house) - deckValue[randomHouse];
+sumSide.hand.innerText = sumHand(hand);
+
+if ((sumHand(house) === 21) & (sumHand(house) === sumHand(hand))) {
+  alert("PUSH");
+} else {
+  if (sumHand(hand) === 21) {
+    alert("Du har Blackjack!");
+  } else {
+    if (sumHand(house) === 21) {
+      alert("Huset har blackjack, du tapte");
+    }
+  }
 }
 
-console.log("hand: "+hand);
-console.log("house: "+house);
-console.log(deck);
+function sumHand(hand) {
+  return hand.reduce((sum, cardName) => sum + deckValue[cardName], 0);
+}
+
+function hit() {
+  cardPull(hand, handDiv);
+
+  if (sumHand(hand) > 21) {
+    console.log("Du har bustet");
+  }
+
+  sumSide.hand.innerText = sumHand(hand);
+}
+
+function stand() {
+  houseDiv.removeChild(handSide);
+  let houseSide = document.createElement("playing-card");
+  houseSide.setAttribute("cid", `${randomHouse}`);
+  houseDiv.appendChild(houseSide);
+
+  while (sumHand(house) < 17) {
+    cardPull(house, houseDiv);
+  }
+
+  if (sumHand(house) > 21) {
+    console.log("Huset har gått bust");
+  }
+
+  sumSide.house.innerText = sumHand(house);
+}
